6e97624f9af1117673153d6c7989b2e251e7eef9
sundaramrap <pragya.learn.28@gmail.com>
2024-10-08 21:08:40 +0530

    addingurlThroughHTML

--- learnBackend/Mern2/index.js
+++ learnBackend/Mern2/index.js
@@ -1,10 +1,33 @@
 const http = require("http");
-const PORT = 8085;
+const PORT = 8084;
+const currenciresData = require("./currencies.json");
 
 const server = http.createServer((req, res) => {
-  console.log("request recieved");
-  res.write(`Server started on :: ${PORT}`);
-  res.end();
+  // CREATING A URL LINK FOR THE SERVER TO RECIEVE REQUEST AND SEND REQUEST
+  switch (req.url) {
+    case "/":
+      res.writeHead(200, { "Content-Type": "application/html" });
+      res.end("<h1>Currency DataBase</h1>");
+      break;
+    case "/currencies":
+      res.writeHead(200, { "Content-Type": "application/html" });
+      res.end(JSON.stringify(currenciresData));
+    default:
+      break;
+  }
+
+  //   console.log("request recieved");
+  //   res.write(`Server started on :: ${PORT}`);
+  //   res.end();
+  //   const infoServer = {
+  //     serverName: "P Server",
+  //     version: "1.0.0",
+  //     currentDate: new Date().toDateString(),
+  //     currentTime: new Date().toTimeString(),
+  //   };
+  //   res.writeHead(200, { "Content-Type": "application/json" });
+  //   res.write(JSON.stringify(infoServer));
+  //   res.end();
 });
 
 server.listen(PORT, () => {
